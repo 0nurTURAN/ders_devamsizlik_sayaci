659c5198b434024a28c641c790441049bb737da6
0nur_TURAN <turan.onur.100@gmail.com>
2026-04-20 22:59:00 +0300

    bitti

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './app'; // app.tsx dosyasını import eder
+import App from './App'; // app.tsx dosyasını import eder
 import './styles/App.css'; // Stil dosyanı import eder
 
 // React'i HTML içerisindeki "root" id'li div'e bağlar
